3006dae020c2017fb921a8beb70ad6c55eeea910
Jockey <jpf_jockey@qq.com>
2018-12-24 22:26:47 +0800

    commandPattern day01

--- src/com/jockey/CommandPattern/Application.java
+++ src/com/jockey/CommandPattern/Application.java
@@ -4,12 +4,50 @@ public interface Application {
 
 }
 
-class Light implements Application{
+
+class GarageDoor implements Application{
+	public void up() {
+		System.out.println("Garage Door is Open");
+	}
+	public void down() {
+		
+	}
+	public void stop() {
+	
+	}
+	public void lightOn() {
+	
+	}
+	public void lightOff() {
+		
+	}
+}
+
+class Stereo implements Application{
+	int volume;
+	
 	public void on() {
+		System.out.println("Stereo is On");
+	}
+
+	public void off() {
+		System.out.println("Stereo is Off");
+	}
+	
+	public void setCD() {
 		
 	}
 	
-	public void off() {
+	public void setDvd() {
 		
 	}
+	
+	public void setRadio() {
+		
+	}
+	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
 }
--- src/com/jockey/CommandPattern/Command.java
+++ src/com/jockey/CommandPattern/Command.java
@@ -4,6 +4,16 @@ public interface Command {
 	public void execute();
 }
 
+
+class NoCommand implements Command{
+
+	@Override
+	public void execute() {
+		
+	}
+	
+}
+
 class LightOnCommand implements Command{
 	Light light;
 	
@@ -18,3 +28,113 @@ class LightOnCommand implements Command{
 	}
 	
 }
+
+class LightOffCommand implements Command{
+	Light light;
+	
+	public LightOffCommand(Light light) {
+		super();
+		this.light = light;
+	}
+
+	@Override
+	public void execute() {
+		light.off();
+	}
+}
+
+
+class GarageDoorOpen implements Command{
+	GarageDoor garageDoor;
+	
+	public GarageDoorOpen(GarageDoor garageDoor) {
+		super();
+		this.garageDoor = garageDoor;
+	}
+
+	@Override
+	public void execute() {
+		garageDoor.up();
+	}
+	
+}
+
+
+class GarageDoorClose implements Command{
+	GarageDoor garageDoor;
+	
+	public GarageDoorClose(GarageDoor garageDoor) {
+		super();
+		this.garageDoor = garageDoor;
+	}
+
+	@Override
+	public void execute() {
+		garageDoor.down();
+	}
+	
+}
+
+class CellingFanHigh implements Command{
+	Fan fan;
+		
+	public CellingFanHigh(Fan fan) {
+		super();
+		this.fan = fan;
+	}
+
+	@Override
+	public void execute() {
+		fan.on();
+	}
+}
+
+
+class CellingFanOff implements Command{
+	Fan fan;
+		
+	public CellingFanOff(Fan fan) {
+		super();
+		this.fan = fan;
+	}
+
+	@Override
+	public void execute() {
+		fan.off();
+	}
+}
+
+class StereoOnForCd implements Command{
+	Stereo stereo;
+	
+	public StereoOnForCd(Stereo stereo) {
+		super();
+		this.stereo = stereo;
+	}
+
+	@Override
+	public void execute() {
+		stereo.on();
+		stereo.setCD();
+		stereo.setVolume(11);
+	}
+	
+}
+
+
+
+class StereoOff implements Command{
+	Stereo stereo;
+	
+	public StereoOff(Stereo stereo) {
+		super();
+		this.stereo = stereo;
+	}
+
+	@Override
+	public void execute() {
+		stereo.off();
+	}
+	
+}
+
